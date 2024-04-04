c0d80d5a940b498fb9f40152315168299a8a866a
Live session user <you@example.com>
2024-04-04 20:10:11 +0000

    final readme file

--- Readme.md
+++ Readme.md
@@ -6,10 +6,14 @@ Clone repo then install deps:
 3 -> pip install SQLAlchemy
 4 -> pip install beautifulsoup4
 5 -> pip install pymemcache
+6 -> sudo apt install memcached -> sudo systemctl start memcached -> sudo systemctl status memcached
+7 -> sudo apt install uvicorn
+
+START server: 
 
 HOW TO CALL APIS:
 
-open localhost:8000/docs to access swagger
+open localhost:8000/docs to access swagger copy curl and import it in postman
 
 1 -> pass authorization = xyz in headers kept it simple auth as for now
 
@@ -20,10 +24,25 @@ description:
     1- no_of_pages is the number of pages you want to pass for scraping
     2- Api will return scrape id through with you can look for scraped products using the second API
 
+SAMPLE CURL: 
+
+curl -X 'GET' \
+  'http://localhost:8000/start-scraping/1' \
+  -H 'authorization: xyz'
+
+
 /scraped-products/{scrape_id}
 description:
     1- scrape_id is the number of pages you want to pass for scraping
     2- Api will return scrape id through with you can look for scraped products using the second API
+
+SAMPLE CURL:
+
+curl -X 'GET' \
+  'http://localhost:8000/scraped-products/1' \
+  -H 'authorization: xyz'
+
+
 
 TO check all data import product.db in DBeaver or any of your fav db viewer
 Execute Select * from products to explore scraped data
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Header
 from models import product
 from database.connection import engine
 from services import scrapeService
@@ -12,28 +12,30 @@ app = FastAPI()
 product.Base.metadata.create_all(bind=engine)
 
 def matchAuthString(auth):
+    print(auth)
     if (AUTH_STRING != auth):
         return False
     return True
 
 #Using main app.py as controller and router for assignment purpoes only!
 @app.get("/start-scraping/{no_of_pages}")
-def startScraping(request: Request, no_of_pages: int):
-    auth = request.headers.get('authorization')
-    isAuth = matchAuthString(auth)
+def startScraping(authorization: str = Header(None), no_of_pages: int = 1):
+    print(authorization)
+    isAuth = matchAuthString(authorization)
 
-    if (isAuth == False) return { "msg": "Unauthorized" }
+    if (isAuth == False):
+        return { "msg": "Unauthorized" }
 
     scrapeId = scrapeService.ScrapeService(no_of_pages).startScraping()
     return { "msg": "To get data of scraped products please use scraped products api with given scrapeId", 'scrape_id': scrapeId }
 
 @app.get("/scraped-products/{scrape_id}")
-def fetchScrapedProducts(request: Request, scrape_id: str):
+def fetchScrapedProducts(authorization: str = Header(None), scrape_id: str = None):
 
-    auth = request.headers.get('authorization')
-    isAuth = matchAuthString(auth)
+    isAuth = matchAuthString(authorization)
 
-    if (isAuth == False) return { "msg": "Unauthorized" }
+    if (isAuth == False): 
+        return { "msg": "Unauthorized" }
 
     print(scrape_id)
     scrapedData = scrapeService.ScrapeService(0).fetchScrapeData(scrape_id)
